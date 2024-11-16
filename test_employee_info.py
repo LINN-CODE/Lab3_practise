16bd0bbe42c7961c0446546bdee557df3bec91ba
Linn <LINNENT.23@ichat.sp.edu.sg>
2024-11-16 20:28:10 +0800

    testing for the employee_info.py

--- test_employee_info.py
+++ test_employee_info.py
@@ -3,3 +3,19 @@ import employee_info as test
 def test_average_salary():
     result = test.calculate_average_salary()
     assert(result == 60166.67)
+
+def test_get_employees_by_age_range():
+    result = test.get_employees_by_age_range(20,30)
+    employee_data = [
+    {"name": "Jane", "age": 25, "department": "Marketing", "salary": 60000},
+    {"name": "Mary", "age": 23, "department": "Marketing", "salary": 56000}
+    ]
+    assert(result == employee_data)
+
+def test_get_employees_by_dept():
+    result = test.get_employees_by_dept("Sales")
+    employee_data = [
+    {"name": "John", "age": 30, "department": "Sales", "salary": 50000},
+    {"name": "Peter", "age": 40, "department": "Sales", "salary": 60000}
+    ]
+    assert(result == employee_data) 
